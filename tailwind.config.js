9ef7a7a83f7762ab2affdabb612b4b7b7d5275ae
Yonathan Antonio <yonathanwizzard@gmail.com>
2022-06-29 00:27:00 +0700

    mobile-screen-fixed

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding : '16px',
+
+    },
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        secondary: '#64748b',
+        dark: '#1e3a8a',
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
